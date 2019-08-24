b7249bad4d00bb85833d1dc48febff9dc8e96e3d
Alexander Thomas <cawfree@Alexanders-MacBook-Pro.local>
2019-08-24 16:14:46 +0100

    Implementing code migration structure.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-native-blobular
 The Man in Blue's awesome Blobular, translated to React Native.
+
+https://www.themaninblue.com/experiment/Blobular/
---
+++ RNBlobular/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Blobular from './components/Blobular';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Blobular
+        
+      />
+    );
+  }
+};
+
+export default App;
